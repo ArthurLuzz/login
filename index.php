dd4004931f1b60b9b34de91ed73d2c3b817c8863
ArthurLuzz <arthur_luz@estudante.sc.senai.br>
2019-10-01 19:53:15 -0300

    CDN jquery Validate

--- index.php
+++ index.php
@@ -44,11 +44,11 @@ abra a URL http://localhost/login/ no navegador fireFrox(Azul)
                 <form action="#" method="post" class="p-2" id="formLogin">
 
                     <div class="form-group">
-                        <input type="text" name="nomeUsuario" id="nomeUsuario" placeholder="Nome de Usuário" class="form-control">
+                        <input type="text" name="nomeUsuario" id="nomeUsuario" placeholder="Nome de Usuário" class="form-control" required minlength="5">
                     </div>
 
                     <div class="form-group">
-                        <input type="password" name="senhaUsuario" id="senhaUsuario" placeholder="senha" class="form-control">
+                        <input type="password" name="senhaUsuario" id="senhaUsuario" placeholder="senha" class="form-control" required minlength="6">
                     </div>
 
                     <div class="from-group">
@@ -97,7 +97,7 @@ abra a URL http://localhost/login/ no navegador fireFrox(Azul)
                     </div>
 
                     <div class="form-group">
-                        <input type="email" name="emailGerarSenha" id="emailGerarSenha" class="form-control" placeholder="E-mail de recuperação da senha">
+                        <input type="email" name="emailGerarSenha" id="emailGerarSenha" class="form-control" placeholder="E-mail de recuperação da senha" required>
                     </div>
 
                     <div class="form-group">
@@ -123,23 +123,23 @@ abra a URL http://localhost/login/ no navegador fireFrox(Azul)
                 <form action="#" method="post" class="p-2" id="formRegistro">
 
                     <div class="form-group">
-                        <input type="text" name="nomeCompleto" id="nomeCompleto" class="form-control" placeholder="Nome completo">
+                        <input type="text" name="nomeCompleto" id="nomeCompleto" class="form-control" placeholder="Nome completo" required minlength="7">
                     </div>
 
                     <div class="form-group">
-                        <input type="text" name="nomeUsuario" id="nomeUsuario" class="form-control" placeholder="Nome de Usuário">
+                        <input type="text" name="nomeUsuario" id="nomeUsuario" class="form-control" placeholder="Nome de Usuário" required minlength="5">
                     </div>
 
                     <div class="form-group">
-                        <input type=" email" name="emailUsuario" id="emailUsuario" class="form-control" placeholder="E-mail">
+                        <input type=" email" name="emailUsuario" id="emailUsuario" class="form-control" placeholder="E-mail" required>
                     </div>
 
                     <div class="form-group">
-                        <input type=" password" name="senhaUsuario" id="senhaUsuario" class="form-control" placeholder="Senha">
+                        <input type="password" name="senhaUsuarioRegistro" id="senhaUsuarioRegistro" class="form-control" placeholder="Senha" required minlength="6">
                     </div>
 
                     <div class="form-group">
-                        <input type="password" name="confirmacaoSenhaUsuario" id="confirmacaoSenhaUsuario" class="form-control" placeholder="Confirmação de Senha">
+                        <input type="password" name="confirmacaoSenhaUsuario" id="confirmacaoSenhaUsuario" class="form-control" placeholder="Confirmação de Senha" required minlength="6">
                     </div>
 
                     <div class="form-group">
@@ -170,9 +170,10 @@ abra a URL http://localhost/login/ no navegador fireFrox(Azul)
     </main>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js""></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.1/jquery.validate.min.js"></script>
     <script>
         //codigo para mostar e ocultar os formulários
         $(function() {
