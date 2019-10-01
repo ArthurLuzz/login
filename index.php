68290f06031a58c836748ad54aeb5a46d5319076
ArthurLuzz <50251309+ArthurLuzz@users.noreply.github.com>
2019-09-30 21:17:53 -0300

    formulário de cadastro de novo usuario finalizado
    close #3

--- index.php
+++ index.php
@@ -14,6 +14,11 @@ abra a URL http://localhost/login/ no navegador fireFrox(Azul)
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <title>Sistema de login</title>
+    <style>
+        #alerta, #caixaSenha, #caixaRegistro{
+            display: none;
+        }
+    </style>
 </head>
 
 <body class="bg-dark">
@@ -64,7 +69,7 @@ abra a URL http://localhost/login/ no navegador fireFrox(Azul)
 
                     <div class="form-group">
                         <p class="text-center">
-                            Novo usuário? <a href="#" id="btnRegistrar">
+                            Novo usuário? <a href="#" id="btnRegistrarNovo">
                                 Registre-se aqui!!
                             </a>
                         </p>
@@ -75,9 +80,9 @@ abra a URL http://localhost/login/ no navegador fireFrox(Azul)
         </section> <!-- Final da Seção de login -->
         <br>
         <!-- Formulario de recuperaçao de senha -->
-        <section class="row">
+        <section class="row mt-5">
 
-            <div class="col-lg-4 offset-lg-4 bg-light rounded" id="caixaLogin">
+            <div class="col-lg-4 offset-lg-4 bg-light rounded" id="caixaSenha">
                 <h2 class="text-center">
                     Gerar Nova Senha
                 </h2>
@@ -123,13 +128,39 @@ abra a URL http://localhost/login/ no navegador fireFrox(Azul)
                         <input type="text" name="nomeUsuario" id="nomeUsuario" class="form-control" placeholder="Nome de Usuário">
                     </div>
 
-                    <div>
-                        <input type="email" name="emailUsuario" id="emailUsuario" class="form-control" placeholder="E-mail">
+                    <div class="form-group">
+                        <input type=" email" name="emailUsuario" id="emailUsuario" class="form-control" placeholder="E-mail">
                     </div>
 
-                    <div>
-                        <input type="password" name="senhaUsuario" id="senhaUsuario" class="form-control" placeholder="Senha">
+                    <div class="form-group">
+                        <input type=" password" name="senhaUsuario" id="senhaUsuario" class="form-control" placeholder="Senha">
                     </div>
+
+                    <div class="form-group">
+                        <input type="password" name="confirmacaoSenhaUsuario" id="confirmacaoSenhaUsuario" class="form-control" placeholder="Confirmação de Senha">
+                    </div>
+
+                    <div class="form-group">
+                        <div class="custom-control custom-checkbox">
+                            <input type="checkbox" name="concordar" id="concordar" class="custom-control-input">
+                            <label for="concordar" class="custom-control-label">
+                                Eu concordo com <a href="#">os termos e condições.</a>
+                            </label>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <input type="submit" value="Registrar" name="btnRegistrar" id="btnRegistrar" class="btn btn-primary btn-block">
+                    </div>
+
+                    <div class="form-group">
+                        <p class="text-center">
+                            Já registrado? <a href="#" id="btnJaRegistrado2">
+                                Entrar por aqui.
+                            </a>
+                        </p>
+                    </div>
+
                 </form>
             </div>
         </section>
@@ -140,6 +171,10 @@ abra a URL http://localhost/login/ no navegador fireFrox(Azul)
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script>
+    //codigo para mostar e ocultar os formulários
+    </script>
+
 </body>
 
 </html>
